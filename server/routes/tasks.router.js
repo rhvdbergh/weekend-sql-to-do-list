e59c088a67e7699ecfb6b15cdd11aa8230ec8fb2
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-15 19:55:27 -0500

    server GET /tasks route retrieves tasks

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,7 +5,25 @@ const pool = require(`../modules/pool.js`);
 router.get(`/`, (req, res) => {
   console.log(`GET /tasks`);
   // TODO: temporary sendStatus
-  res.sendStatus(200);
+
+  // build SQL query - select everything!
+  let query = `SELECT * FROM "tasks"`;
+
+  pool
+    .query(query)
+    .then((response) => {
+      console.log(`received pg response; rows:`, response.rows);
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      handleError(err);
+      res.sendStatus(500);
+    });
 });
+
+// error handler for all errors
+function handleError(err) {
+  console.log(`There was an error connecting to PostgreSQL:`, err);
+}
 
 module.exports = router;
