58e58b5a35ff1f43c212d401a1d33872e4d37908
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-15 20:19:52 -0500

    renders tasks to DOM, including class; removes dummy data from index.html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,5 +30,30 @@ function getTasks() {
 }
 
 function render(tasks) {
-  console.log(tasks);
+  for (let task of tasks) {
+    // build row as a jQuery object
+    let row = $(`
+    <tr class="${
+      task.complete
+        ? // conditional: add .complete or .incomplete depending on task
+          'taskComplete'
+        : 'taskIncomplete'
+    }">
+      <td class="taskIncomplete">${task.task}</td>
+      <td> 
+        ${
+          task.complete
+            ? // conditional: complete button only present when task is not complete
+              ''
+            : '<button id="completeBtn" class="btn btn-success">Complete</button>'
+        }
+      </td>
+      <td>
+        <button id="completeBtn" class="btn btn-danger">Delete</button>
+      </td>
+    </tr>`);
+    // set the id for this task on the tr
+    row.data(`id`, task.id);
+    $(`#tasksDisplayTableBody`).append(row);
+  } // end for
 }
