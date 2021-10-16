7129805cf49dbba3fd3236a38b4850086f606e28
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-15 20:09:35 -0500

    ajax GET /tasks retrieves tasks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,30 @@ $(onReady);
 
 function onReady() {
   console.log(`in jq`);
+  getTasks();
+  // set up event listeners
+  attachEventListeners();
+}
+
+function attachEventListeners() {}
+
+// retrieve all tasks (and render() )
+function getTasks() {
+  $.ajax({
+    method: `GET`,
+    url: `/tasks`,
+  })
+    .then(function (response) {
+      render(response);
+    })
+    .catch(function (err) {
+      console.log(
+        `There was an error fetching the tasks from the server:`,
+        err
+      );
+    });
+}
+
+function render(tasks) {
+  console.log(tasks);
 }
